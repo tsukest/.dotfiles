e95635fbaf2c53e3dd8f1df2b66d4384d198726b
satake <satake.ts@gmail.com>
2019-12-27 15:22:39 +0000

    fix

--- check.sh
+++ check.sh
@@ -3,12 +3,22 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly REPO_ROOT="$(realpath $(dirname $0))"
-readonly DOTFILES=$(find ./ -type f -name '.*' | xargs -I{} basename {})
-
+readonly ROOT_PATH="$(realpath $(dirname $0))"
+readonly DOTFILES=$(find ${ROOT_PATH} -type f -name '.*' | xargs -I{} basename {})
 for dotfile in ${DOTFILES}; do
   dotfilename=$(basename "${dotfile}")
   linkfile="${HOME}/${dotfilename}"
+  message="exist"
+  if [[ ! "$(ls -l ${linkfile} 2> /dev/null | cut -c 1)" == "l"  ]]; then
+    message="not ${message}"
+  fi
+  printf "%-9s    %s\n" "${message}" "${linkfile}" 1>&2
+done
+
+readonly CONFIG_FILES=$(find ${ROOT_PATH}/.config/* -type f)
+for config_file in ${CONFIG_FILES[@]}; do
+  relative_file_path="$(echo "${config_file}" | sed -r 's/.*\/(.config\/.*)$/\1/')"
+  linkfile="${HOME}/${relative_file_path}"
   message="exist"
   if [[ ! "$(ls -l ${linkfile} 2> /dev/null | cut -c 1)" == "l"  ]]; then
     message="not ${message}"
--- setup.sh
+++ setup.sh
@@ -3,15 +3,29 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-readonly ROOT_DIR=$(realpath $(dirname $0))
-readonly DOTFILES=$(find ./ -type f -name '.*' | xargs -I{} basename {})
-
+readonly ROOT_PATH="$(realpath $(dirname $0))"
+readonly DOTFILES=$(find ${ROOT_PATH} -type f -name '.*' | xargs -I{} realpath {})
 for dotfile in ${DOTFILES[@]}; do
-  TARGET="${ROOT_DIR}/${dotfile}"
-  LINK_NAME="${HOME}/${dotfile}"
+  TARGET="${dotfile}"
+  LINK_NAME="${HOME}/$(basename ${dotfile})"
 
   if [[ -f "${LINK_NAME}" ]]; then
     echo "${dotfile} aready exists" 1>&2
+    continue
+  fi
+  ln -s "${TARGET}" "${LINK_NAME}"
+  echo "create ${LINK_NAME}" 1>&2
+done
+
+readonly CONFIG_FILES=$(find ${ROOT_PATH}/.config/* -type f)
+for config_file in ${CONFIG_FILES[@]}; do
+  RELATIVE_FILE_PATH="$(echo "${config_file}" | sed -r 's/.*\/(.config\/.*)$/\1/')"
+  mkdir -p "${HOME}/$(dirname ${RELATIVE_FILE_PATH})"
+  TARGET="${config_file}"
+  LINK_NAME="${HOME}/${RELATIVE_FILE_PATH}"
+
+  if [[ -f "${LINK_NAME}" ]]; then
+    echo "${config_file} aready exists" 1>&2
     continue
   fi
   ln -s "${TARGET}" "${LINK_NAME}"
